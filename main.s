0b0f7b70ccace6e16c1a3becfbbcf971a9e1afa7
Ollie Hockey <oh123@ic.ac.uk>
2026-02-27 11:41:12 +0000

    Refactor main.s for gyro SPI test and LED output

--- main.s
+++ main.s
@@ -1,27 +1,57 @@
-	#include <xc.inc>
+;========================
+; main.s  (LSM9DS1 gyro SPI quick test)
+; PIC18F87K22
+;========================
+#include <xc.inc>
 
-psect	code, abs
-	
-main:
-	org	0x0
-	goto	start
+; --- exports from imu module ---
+extrn   IMU_SPI_Init
+extrn   IMU_Gyro_WhoAmI_Test
+extrn   IMU_Gyro_Config
+extrn   IMU_Gyro_Read_XL
 
-	org	0x100		    ; Main code starts here at address 0x100
+; --- simple delay from your LCD module (or replace with your own) ---
+extrn   LCD_delay_ms
+
+psect   code, abs
+rst:    org     0x0000
+        goto    start
+
+psect   code
 start:
-	movlw	0xFF
-	movwf	TRISD, A
-	movlw 	0x0
-	movwf	TRISC, A
-	movlw 	0x0		; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTC
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movf	PORTD, W, A
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+        ; --- PORTJ LEDs for visible output ---
+        clrf    TRISJ, A
+        clrf    LATJ,  A
 
-	end	main
+        ; --- SPI init + WHO_AM_I test ---
+        call    IMU_SPI_Init
+        call    IMU_Gyro_WhoAmI_Test      ; returns W = WHO_AM_I_XG
+        movwf   LATJ, A                  ; show WHO_AM_I on LEDs
+
+        ; if WHO_AM_I != 0x68, blink all LEDs fast forever
+        movlw   0x68
+        cpfseq  LATJ, A
+        bra     bad_sensor
+
+        ; --- configure gyro then stream X low byte to LEDs ---
+        call    IMU_Gyro_Config
+
+loop:
+        call    IMU_Gyro_Read_XL         ; returns W = OUT_X_L_G
+        movwf   LATJ, A                  ; show changing byte on LEDs
+
+        movlw   50
+        call    LCD_delay_ms
+        bra     loop
+
+bad_sensor:
+        movlw   0xFF
+        movwf   LATJ, A
+        movlw   100
+        call    LCD_delay_ms
+        clrf    LATJ, A
+        movlw   100
+        call    LCD_delay_ms
+        bra     bad_sensor
+
+end rst
