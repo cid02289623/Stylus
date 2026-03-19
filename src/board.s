87bad977ef60a1a77bb936d5c8382bf92e118e8b
Ollie Hockey <oh123@ic.ac.uk>
2026-03-19 21:20:47 +0000

    Delete src/board.s

--- src/board.s
+++
@@ -1,9 +0,0 @@
-            PROCESSOR 18F87K22
-            #include <xc.inc>
-
-;===========================================================
-; board.s
-; Reserved for future board-level init routines.
-;===========================================================
-
-            end
